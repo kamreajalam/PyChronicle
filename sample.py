97bb70e20aa5bbe60bb5629ca38431de6b1b8d63
Raguvaran T <your_email@gmail.com>
2026-07-23 12:46:14 +0530

    Update sample code

--- sample.py
+++ sample.py
@@ -1,34 +1,71 @@
-import math
-
-x = 10
-y = 20
-sum_value = x + y
-
-
-def greet(name):
-    return f"Hello, {name}"
+print("ATM TRANSACTION")
+# atm transaction
+class ATM:
+    
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
 
 
-class Student:
-    def __init__(self, name):
-        self.name = name
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds")
+        else:
+            self.balance -= amount
+            print(f"Withdrawn: {amount}. New balance: {self.balance}")
 
-    def introduce(self):
-        print(greet(self.name))
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited: {amount}. New balance: {self.balance}")
+    def get_balance(self):
+        return self.balance
+    def get_account_number(self):
+        return self.account_number
+    def set_pin(self, pin):
+        self.pin = pin
+    def generate_receipt(self):
+        print(f"Account Number: {self.account_number}, Balance: {self.balance}")
 
-
-for i in range(3):
-    print(i)
-
-while x > 0:
-    print(x)
-    x -= 1
-
-if sum_value > 20:
-    print("sum is large")
+account_number = input("Enter your account number: ")
+ac={
+    "123456789": 1000,
+    "234555": 2000
+}
+if account_number in ac:
+        initial_balance = ac[account_number]
 else:
-    print("sum is small")
+        print("Invalid account number.")
+        exit()
 
-student = Student("Alice")
-student.introduce()
-print(math.sqrt(sum_value))
+
+#create an instance of the ATM object with the provided account number and initial balance
+a1 = ATM(account_number, initial_balance)
+
+print(f"Account Number: {a1.get_account_number()}, Balance: {a1.get_balance()}")
+if __name__ == "__main__":
+    while True:
+        print("\nOptions:")
+        print("1. Withdraw")
+        print("2. Deposit")
+        print("3. Exit")
+        print("4. Set PIN")
+     
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            amount = float(input("Enter amount to withdraw: "))
+            a1.withdraw(amount)
+        elif choice == '2':
+            amount = float(input("Enter amount to deposit: "))
+            a1.deposit(amount)
+        elif choice == '3':
+            print("Exiting...")
+            print("-----------")
+            break
+        elif choice == '4':
+            pin = input("Enter new PIN: ")
+            a1.set_pin(pin)
+        else:
+            print("Invalid choice. Please try again.")
+print(f"Final Balance: {a1.get_balance()}")
+print("Thank you for using the ATM.")
